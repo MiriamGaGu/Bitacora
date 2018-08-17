a36c879229d02f70adae02cc649c1a35f07c4981
Miriam <mgg_pjl@yahoo.com>
2018-08-17 18:09:42 -0500

    Iniciando mi repo

---
+++ src/index.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBcPBABIKN8QgQFcFT0BvIOwRlZbFOrTfk",
+    authDomain: "bitacora-91871.firebaseapp.com",
+    databaseURL: "https://bitacora-91871.firebaseio.com",
+    projectId: "bitacora-91871",
+    storageBucket: "bitacora-91871.appspot.com",
+    messagingSenderId: "421712651428"
+  };
+  firebase.initializeApp(config);
